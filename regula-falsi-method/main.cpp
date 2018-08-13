de778fcc527a2db2b1efc0cfe4f702748d004d3c
Pritom Gogoi <kickstart7962@gmail.com>
2018-08-13 14:40:36 +0530

    add regula-falsi-method

---
+++ regula-falsi-method/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+#include <iomanip>
+using namespace std;
+
+double compute(double x){
+    return sin(x) - (1/x);
+}
+int main()
+{
+    double a,b,c;
+    double limit;
+    double func_c;
+    int iter=0;
+
+    cout<<"Enter left boundary value, a: ";
+    cin>>a;
+    cout<<"Enter right boundary value, b: ";
+    cin>>b;
+
+    double func_a=compute(a);
+    double func_b=compute(b);
+    cout<<endl;
+
+    if(func_a/fabs(func_a)*(func_b/fabs(func_b))<0){
+        cout<<"[Msg] roots exist in the given boundary";
+    }else{
+        cout<<"[Msg] root doesn't exist in the given boundary";
+        exit(0);
+    }
+    cout<<endl;
+
+    cout<<"\nEnter required accuracy: ";
+    cin>>limit;
+
+    cout<<"Iter"<<setw(14)<<"a"<<setw(18)<<"b"<<setw(18)<<"c"<<setw(18)<<"f(c)"<<setw(18)<<endl;
+    cout<<"------------------------------------------------------------------------------"<<endl;
+    while(abs(a-b)>limit){
+        iter++;
+        c=(a*func_b-b*func_a)/(func_b-func_a);
+        func_c=compute(c);
+        cout<<setw(3)<<iter<<setw(18)<<a<<setw(18)<<b<<setw(18)<<c<<setw(18)<<func_c<<endl;
+
+        if(func_c==0){
+            break;
+        }
+        else if((func_c/fabs(func_c))*(func_a/fabs(func_a))<0)
+            b=c;
+        else{a=c;}
+    }
+    cout<<endl<<"The root of the equation is "<<c;
+    return 0;
+}
+
+
+
